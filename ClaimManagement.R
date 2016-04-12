6a503e4176647cf7bf3bd0952f6298c4c9a50e84
Jeeva-G <jeeva.analyst@gmail.com>
2016-04-12 12:32:39 +1200

    Loading libraries and data

--- ClaimManagement.R
+++ ClaimManagement.R
@@ -1,1 +1,7 @@
-#Load the data
+#Load the libraries
+library(xgboost)
+
+# load training, testing, and sample_submission data sets..
+train <- read.table("../inputdata_github/train.csv", sep = ",", header = TRUE)
+test <- read.table("../inputdata_github/test.csv", sep = ",", header = TRUE)
+sample_submission <- read.table("../inputdata_github/sample_submission.csv", sep = ",", header = TRUE)
